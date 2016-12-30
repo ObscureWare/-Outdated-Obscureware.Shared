70b8e7d1bedc0ff1f943e747ae01528e31000ef0
Sebastian Gruchacz <sebastian@gruchacz.com>
2016-12-30 22:39:11 +0100

    Separating really shared content taht is not going into separate nugget. Separate testing and documentary will follow soon.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Obscureware.Shared
-No nuget from this ;-) Just my shared stuff - testing, helpers, extensions.
+No nuget from this ;-) (Yet?) Just my shared stuff - testing, helpers, extensions.
+
+You need this library to compile my other projects.
---
+++ src/Obscureware.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Obscureware.Shared")]
+[assembly: AssemblyDescription("Shared helpers and extensions library. Mostly for testing purposes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Obscureware Solutions")]
+[assembly: AssemblyProduct("Obscureware.Shared")]
+[assembly: AssemblyCopyright("Copyright © Sebastian Gruchacz 2016-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("286f5fe3-b75f-41b0-98bf-095f4617513a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
